8ea98bb647868fd86f5512e2aed5bbb8f8fc5845
Pavithra <pavithracsb175@gmail.com>
2024-11-22 14:48:15 +0530

    Initial commit: Added Freelance Marketplace OOAD project

---
+++ src/com/jobportal/application/models/DB_VARIABLES.java
@@ -0,0 +1,11 @@
+package com.jobportal.application.models;
+
+public interface DB_VARIABLES {
+    String DB = "job_portal";
+    String HOST = "localhost";
+    String PASSWORD = "8query";
+    String PORT = "3306";
+    String URI = "mysql://root:8query@localhost:3306/job_portal";
+    String USER = "root";
+    String VERSION = "8.0";
+}
